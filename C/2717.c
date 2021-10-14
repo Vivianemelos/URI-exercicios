ae2c52dbb4b6cf4f2eccb62b3be51c88bdc4efc0
Viviane Sousa de Melo <vivianemeloscc@gmail.com>
2021-10-13 23:50:09 -0300

    Exercício em C

---
+++ C/2717.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(){
+
+  int n,a,b;
+  scanf("%d",&n);
+  scanf("%d %d", &a, &b);
+  
+ 
+  if(n<(a+b)){
+     printf("Deixa para amanha!\n");}
+  else
+     {printf("Farei hoje!\n");}
+    return 0;
+}
